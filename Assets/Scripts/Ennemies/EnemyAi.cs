e29ddd8f53d549e93a291289183ca582553a6269
Tom Rives <tom.rives@epitech.eu>
2022-11-25 15:56:31 +0900

    [ADD] Basic Enemmies AI that follow the player

---
+++ Assets/Scripts/Ennemies/EnemyAi.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public enum EnemyState
+{
+    Chase,
+    Attack
+}
+
+public class EnemyAi : MonoBehaviour
+{
+    public NavMeshAgent Agent;
+    public Transform Player;
+    public LayerMask WhatIsGround, WhatIsPlayer;
+
+
+    public float AttackRange = 1;
+    private bool PlayerInAttackRange;
+
+    //Attack
+    public float timeDelayAttack;
+    public float Damage;
+    public bool AlreadyAttack;
+
+    public EnemyState State = EnemyState.Chase;
+
+    void Awake()
+    {
+        Player = GameObject.Find("Player").gameObject.transform;
+        Agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerInAttackRange = Physics.CheckSphere(transform.position, AttackRange, WhatIsPlayer);
+
+        if (PlayerInAttackRange)
+            AttackPlayer();
+        else
+            ChasePlayer();
+    }
+
+    private void ChasePlayer()
+    {
+        Agent.SetDestination(Player.position);
+    }
+
+    private void AttackPlayer()
+    {
+        if (AlreadyAttack)
+            return;
+        Debug.Log("Attack !");
+        AlreadyAttack = true;
+        Invoke(nameof(ResetAttack), timeDelayAttack);
+    }
+
+    private void ResetAttack()
+    {
+        AlreadyAttack = false;
+    }
+}
