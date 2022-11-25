77a8c5ce3966e763552befd053edd249145692f5
unknown <alan.sigal@epitech.eu>
2022-11-26 01:01:00 +0900

    [ADD] character

---
+++ Assets/Scripts/Character/ShootHandler.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootHandler : MonoBehaviour
+{
+    public GameObject BulletPrefab;
+
+    public Transform BulletSpawnPoint;
+    
+    public Transform CharacterPosition;
+
+    public float BulletSpeed = 1000f;
+
+    public float FireRate = 0.5f;
+
+    private float _nextFire = 0f;
+
+
+    public void Shoot()
+    {
+        if (Time.time > _nextFire)
+        {
+            _nextFire = Time.time + FireRate;
+
+            var bullet = Instantiate(BulletPrefab, BulletSpawnPoint.position, CharacterPosition.rotation);
+
+            bullet.GetComponent<Rigidbody>().AddForce(bullet.transform.forward * BulletSpeed, ForceMode.Impulse);
+
+            Destroy(bullet, 2.0f);
+        }  
+    }
+}
