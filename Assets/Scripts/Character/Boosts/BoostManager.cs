113759dd226def876252f716d7436c4c8cafc43e
unknown <alan.sigal@epitech.eu>
2022-12-02 22:50:22 +0900

    [ADD] boost system

---
+++ Assets/Scripts/Character/Boosts/BoostManager.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BoostManager : MonoBehaviour
+{
+    private BoostType[] _boostTypes = { BoostType.Speed, BoostType.AttackSpeed, BoostType.Damage, BoostType.SpecialAttack };
+
+    public BoostType[] GenerateRandomBoosts()
+    {
+        BoostType[] boosts = new BoostType[3];
+        List<BoostType> generatedBoosts = new List<BoostType>();
+
+        for (int i = 0; i < 3; i++)
+        {
+            BoostType randomBoost = GetRandomBoost();
+            if (!generatedBoosts.Contains(randomBoost))
+            {
+                boosts[i] = randomBoost;
+                generatedBoosts.Add(randomBoost);
+            }
+            else
+            {
+                i--;
+            }
+        }
+
+        return boosts;
+    }
+
+    private BoostType GetRandomBoost()
+    {
+        int randomIndex = Random.Range(0, _boostTypes.Length);
+        return _boostTypes[randomIndex];
+    }
+
+    public void ChooseBoost(BoostType[] boosts, int boostIndex)
+    {
+        if (boostIndex >= 0 && boostIndex < boosts.Length)
+        {
+            BoostType chosenBoost = boosts[boostIndex];
+            ApplyBoost(chosenBoost);
+        }
+        else
+        {
+            Debug.LogError("Invalid boost index: " + boostIndex);
+        }
+    }
+
+    public void ApplyBoost(BoostType boost)
+    {
+        switch (boost)
+        {
+            case BoostType.Speed:
+                break;
+            case BoostType.AttackSpeed:
+                break;
+            case BoostType.Damage:
+                break;
+            case BoostType.SpecialAttack:
+                break;
+            default:
+                Debug.LogError("Invalid boost type: " + boost);
+                break;
+        }
+    }
+}
